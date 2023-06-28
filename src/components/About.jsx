7a8e37a4b12fc2469ab80cf727a8a9ff3debc9c7
yennhi26 <yennhi97263@gmail.com>
2023-06-28 23:24:53 +0700

    add About component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,23 +10,31 @@ import { SectionWrapper } from '../hoc'
 
 const ServiceCard =  ({index, title, icon}) => {
   return (
-    <Tilt className="xs:w-[250px] w-full">
-      <motion.div
-      variants={fadeIn("right", "spring", 0.5 * index, 0.75)} className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
-      >
-        <div
+    <Tilt className='xs:w-[250px] w-full'>
+    <motion.div
+      variants={fadeIn("right", "spring", index * 0.5, 0.75)}
+      className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
+    >
+      <div
         options={{
           max: 45,
           scale: 1,
-          speed: 450
+          speed: 450,
         }}
-        className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col"
-        >
-          <img src={icon} alt={title} className="w-16 h-16 object-contain" />
-          <h3 className="text-white text-[20px] font-bold text-center">{title}</h3>
-        </div>
-      </motion.div>
-    </Tilt>
+        className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
+      >
+        <img
+          src={icon}
+          alt='web-development'
+          className='w-16 h-16 object-contain'
+        />
+
+        <h3 className='text-white text-[20px] font-bold text-center'>
+          {title}
+        </h3>
+      </div>
+    </motion.div>
+  </Tilt>
   )
 }
 
@@ -39,8 +47,8 @@ const About = () => {
       </motion.div>
 
       <motion.p
-      variants={fadeIn("", "", 0.1, 1)}
-      className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
+        variants={fadeIn("", "", 0.1, 1)}
+        className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
       >
         I&apos;m a Frontend developer with experience in TypeScript and
         JavaScript, and expertise in frameworks like React, Angular, and
@@ -52,7 +60,7 @@ const About = () => {
         real-world problems. Let&apos;s work together to bring your ideas to life!
       </motion.p>
 
-      <div className="mt-20 flex flex-wrap gap-10">
+      <div className='mt-20 flex flex-wrap gap-10'>
         {services.map((service, index) => (
           <ServiceCard key={service.title} index={index} {...service} />
         ))}
